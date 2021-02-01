d61230465ff03753f93d65f87806bf9f2f0ff57d
Colin Brooks <colinrobertbrooks@gmail.com>
2021-02-01 09:49:22 -0500

    browser language detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12">
+        <h1 className="mt-4 text-center">Browser Language Detection</h1>
+        <hr />
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Source</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <code>navigator.language</code>
+              </td>
+              <td>{navigator.language}</td>
+            </tr>
+            <tr>
+              <td>
+                <code>navigator.languages</code>
+              </td>
+              <td>{JSON.stringify(navigator.languages)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
